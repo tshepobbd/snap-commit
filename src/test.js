2b6c873e373c8cc1c6d0771204fb09a4c258ee6f
tshepobbd <tshepobbd@bbd.co.za>
2025-08-23 16:43:22 +0200

    chore: Update test.js with new environment variable logging

--- src/test.js
+++ src/test.js
@@ -3,3 +3,4 @@ import dotenv from "dotenv";
 dotenv.config();
 
 console.log("OPENAI_API_KEY:", process.env.OPENAI_API_KEY);
+console.log("SNAP_COMMIT_BACKEND_URL:", process.env.SNAP_COMMIT_BACKEND_URL);
